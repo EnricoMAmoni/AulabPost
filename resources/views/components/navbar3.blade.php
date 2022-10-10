6ccf8cd9ebee6aa3b249c9f8713c8ac55011028d
Gabriele Palazzo <gabrielepalazzo98@gmail.com>
2022-10-10 13:01:16 +0200

    completata navbar di ricerca per categorie

--- resources/views/components/navbar3.blade.php
+++ resources/views/components/navbar3.blade.php
@@ -1,31 +1,21 @@
-<nav class="navbar nav3 navbar-expand-lg pt-0">
+<nav class="navbar nav3 navbar-expand-lg  p-0 ">
 
-<div class="container-fluid nav5 ">
+<div class="container-fluid nav5 align-items-center">
       
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="collapse navbar-collapse " id="navbarNav">
         <ul class="navbar-nav nav3">
+
+          @foreach ($categories as $category)
+          
           <li class="nav-item">
-            <a class="nav-link active textNav1 navt1" aria-current="page" href="#">Politica</a>
+            <a class="nav-link active textNav1 navt1" aria-current="page" href="{{route('articles.category', compact('category'))}}">{{$category->name}}</a>
           </li>
-        
-          <li class="nav-item">
-            <a class="nav-link textNav1 navt1" href="#">Economia</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link textNav1 navt1" href="#">Food</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link textNav1 navt1" href="#">Sport</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link textNav1 navt1" href="#">Intrattenimento</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link textNav1 navt1" href="#">Tech</a>
-          </li>
+          
+          @endforeach
+      
         </ul>
     </div>   
     

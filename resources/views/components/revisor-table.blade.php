de2c984c4adad5d775371029007067196d06ddcb
luca vignoli <lucavignoli97@gmail.com>
2022-10-12 18:24:12 +0200

    dashboard revisore resa responsive

--- resources/views/components/revisor-table.blade.php
+++ resources/views/components/revisor-table.blade.php
@@ -1,8 +1,76 @@
-<table class="table">
+<div class="container mt-5 mb-5 container-revisore">
+    <div class="row align-items-center ">
+        
+        @foreach ($articles as $article)
+
+        <div class="col-12 col-md-2 ">
+            <h3 class="mt-2">Id</h3>
+            <p class=" mt-3">{{$article->id}}</p>
+            
+        </div>
+        <div class="col-12 col-md-2">
+            <h3 class="mt-2">Titolo</h3>
+            <p class=" mt-3">{{$article->title}}</p>
+            
+        </div>
+        <div class="col-12 col-md-2">
+            <h3 class="mt-2">Categoria</h3>
+            <p class=" mt-3">{{$article->category->name}}</p>
+            
+        </div>
+        <div class="col-12 col-md-2">
+            <h3 class="mt-2">Scritto da</h3>
+            <p class=" mt-3">{{$article->user->name}}</p>
+            
+        </div>
+        <div class="col-12 col-md-2">
+            <h3 class="mt-2" >scritto il</h3>
+            <p class=" mt-3">{{$article->created_at->format('Y-m-d')}}</p>
+            
+        </div>
+        <div class="col-12 col-md-2">
+            <h3 class="mt-2">leggi</h3>
+            <a href="{{route('revisor.detail', $article)}}" class="btn mb-3">Leggi</a>
+            
+        </div>
+        <hr>
+        @endforeach  
+        
+           
+            
+            
+            
+           
+            
+        
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{{-- <table class="table">
    <thead>
         <tr>
             <th scope="col">#</th>
-            <th scope="col">Titolo</th>
+            <th scope="col">titolo</th>
             <th scope="col">Categoria</th>
             <th scope="col">Scritto da</th>
             <th scope="col">Scritto il</th>
@@ -12,9 +80,9 @@
    </thead>
 
    <tbody>
-    @foreach ($articles as $article)
+    
         <tr>
-            <th scope="row">{{$article->id}}</th>
+            <th scope="row"></th>
              <td>{{$article->title}}</td>
              <td>{{$article->category->name}}</td>
              <td>{{$article->user->name}}</td>
@@ -25,6 +93,6 @@
             
         </tr>
         
-    @endforeach
+    
    </tbody>
-</table>
+</table> --}}
